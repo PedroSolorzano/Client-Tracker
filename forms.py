fe51819bad327250f3d8bc2dda0699d22844944e
Keivou <keivou@desktop.local>
2023-02-13 19:02:50 -0500

    - add client info form and user info form - add login functionality - add logout functionality - add bcrypt to requirements.txt - add fresh_login_required decorator - add logout button to sidebar - remove most info from profile.html - update form in profile.html - update sign-in.html and sign-up.html - redesign users table in database - logout is hidden for logged-out users (may not be working)

--- forms.py
+++ forms.py
@@ -1,18 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired
 
 
 # WTForms
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign me up!")
+    submit = SubmitField("Sign up")
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
+
+
+class UserInformationForm(FlaskForm):
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email address", validators=[DataRequired()])
+    about_me = StringField("About Me")
+    submit = SubmitField('Submit Changes')
+    cancel = SubmitField('Cancel')
+
+
+class ClientInformationForm(FlaskForm):
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email address", validators=[DataRequired()])
+    address = StringField("Address")
+    city = StringField("City")
+    country = StringField("Country")
+    postal_code = StringField("Postal Code")
+    about_me = StringField("About Me")
+    submit = SubmitField('Submit Changes')
+    cancel = SubmitField('Cancel')
 
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask import Flask, render_template, redirect, url_for, flash, request, abort, session
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import LoginForm, RegisterForm
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, fresh_login_required, logout_user
+from forms import LoginForm, RegisterForm, UserInformationForm, ClientInformationForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from sqlalchemy import ForeignKey
 from flask_bcrypt import Bcrypt
+import bcrypt
 import os
 
 # Flask app and bootstrap
@@ -42,7 +42,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(1000), nullable=False)
+    username = db.Column(db.String(1000), nullable=False)
+    first_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=True)
     about_me = db.Column(db.String(1000), nullable=True)
     # Relationships
     clients = relationship("Client", back_populates="user")
@@ -102,27 +104,33 @@ def home_page():
 
 
 @app.route('/billing')
+@fresh_login_required
 def billing_page():
     return render_template('billing.html')
 
 
-@app.route('/profile')
+@app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile_page():
-    return render_template('profile.html')
+    user_form = UserInformationForm()
+    if request.method == 'POST' and user_form.validate_on_submit() and user_form.submit.data:
+        user = User.query.get(current_user.id)
+        # Change user's data
+        user.username = user_form.username.data
+        user.first_name = user_form.first_name.data
+        user.last_name = user_form.last_name.data
+        user.about_me = user_form.about_me.data
+        # for changing the email, it must first be checked that the new email doesn't already exist
+        taken_email = User.query.filter(email=user_form.email.data).first()
+        if not taken_email:
+            user.email = user_form.email.data
+        return redirect(url_for('profile.html', form=user_form))
+    elif request.method == 'POST' and user_form.cancel.data:
+        return redirect(url_for('profile.html', form=user_form))
+    return render_template('profile.html', form=user_form)
 
 
-@app.route('/login')
-def login_page():
-    return render_template('sign-in.html')
-
-
-@app.route('/logout')
-def logout():
-    return render_template('sign-in.html')
-
-
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     login_form = LoginForm()
     register_form = RegisterForm()
@@ -132,7 +140,7 @@ def register_page():
         bcrypt_pwd = bcrypt.generate_password_hash(register_form.password.data)
         new_user = User(email=register_form.email.data,
                         password=bcrypt_pwd,
-                        name=register_form.name.data)
+                        username=register_form.name.data)
         db.session.add(new_user)
         db.session.commit()
         # Login user
@@ -143,6 +151,41 @@ def register_page():
         return redirect(url_for('login_page', form=login_form, logged_in=current_user.is_authenticated))
     else:
         return render_template('sign-up.html', form=register_form, logged_in=current_user.is_authenticated)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    login_form = LoginForm()
+    register_form = RegisterForm()
+    if request.method == 'GET':
+        return render_template("sign-in.html", form=login_form, logged_in=current_user.is_authenticated)
+
+    if request.method == 'POST' and login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+
+        if not user:
+            flash("This email doesn't exist, please register first.")
+            return redirect(url_for('register_page', form=register_form, logged_in=current_user.is_authenticated))
+        elif current_user.is_active:
+            flash('User already logged in.')
+            return redirect(url_for('login_page', form=login_form, logged_in=current_user.is_authenticated))
+        elif user and bcrypt.check_password_hash(user.password, login_form.password.data):
+            # Login user
+            login_user(user)
+            return redirect(url_for('profile_page', logged_in=current_user.is_authenticated))
+        else:
+            flash('Incorrect password, please try again.')
+            return redirect(url_for('login_page', form=login_form, logged_in=current_user.is_authenticated))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    if current_user.is_authenticated:
+        logout_user()
+        return redirect(url_for('login_page', logged_in=current_user.is_authenticated))
+    else:
+        return redirect(url_for('login_page', logged_in=current_user.is_authenticated))
 
 
 @app.route('/clients')
